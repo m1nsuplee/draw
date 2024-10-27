1d70ee189bdf1156897549db830ddf86530ed3e7
Minsup Lee <m1nsuppp@gmail.com>
2024-10-27 14:54:52 +0900

    [refactor] button id 상수로 관리

---
+++ src/constants/button-ids.ts
@@ -0,0 +1,4 @@
+export const BUTTON_IDS = {
+  pencil: 'pencil-button',
+  eraser: 'eraser-button',
+};
--- src/lib/tool-buttons.ts
+++ src/lib/tool-buttons.ts
@@ -1,8 +1,9 @@
+import { BUTTON_IDS } from '../constants/button-ids';
 import type { Tool } from '../models/tool';
 import { getButtonElementById } from './utils';
 
-const pencilButton = getButtonElementById('pencil-button');
-const eraserButton = getButtonElementById('eraser-button');
+const pencilButton = getButtonElementById(BUTTON_IDS.pencil);
+const eraserButton = getButtonElementById(BUTTON_IDS.eraser);
 
 export const toolButtons: (HTMLButtonElement & { toolType: Tool['type'] })[] = [
   Object.assign(pencilButton, { toolType: 'pencil' as Tool['type'] }),
